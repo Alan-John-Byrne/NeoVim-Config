3b56f88fe85cbb07214f1d1251bfe463b7740805
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2024-10-11 00:39:31 +0100

    feat(lsp): add basic demo for lsp configurations

---
+++ lua/config/lspconfigs.lua
@@ -0,0 +1,8 @@
+-- IMPORTANT: Java's LSP (nvim-jdtls) works independently from nvim-lspconfig. NOT INCLUDED HERE.
+
+-- NOTE: EXAMPLE
+-- local lspconfig = require("lspconfig")
+-- lspconfig.gopls.setup({
+-- settings = {
+-- *settings got to do with the golang lsp*
+-- }
