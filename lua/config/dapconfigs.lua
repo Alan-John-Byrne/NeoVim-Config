1947d13eae93bc1f9e69300095776174c390cf14
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2024-10-20 23:59:51 +0100

    feat(debug) Add debug support for python.
    I've configured the 'debugpy' debugger for python files. This was done
    via the package installed via mason in the DAP section.

--- lua/config/dapconfigs.lua
+++ lua/config/dapconfigs.lua
@@ -1,11 +1,11 @@
-local dap = require("dap")
+local dap = require("dap") -- WARN: REQUIRED FOR ALL LANGUAGE SETUPS.
 
 -- TODO: C++ DEBUG ADAPTER CONFIGURATION:
 dap.adapters.cppdbg = { -- NOTE: HERE WE ARE CREATING A DEBUG ADAPTER 'TYPE' WE WILL USE BELOW WITHIN A CONFIGURATION.
   id = "cppdbg",
   type = "executable", -- Because we are using the debugger's executable program.
   -- IMPORTANT: The actual path to the cpp OpenDebugAD7 debugger.
-  command = "C:\\Users\\alanj\\.vscode\\extensions\\ms-vscode.cpptools-1.21.6-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe",
+  command = "C:\\Users\\alanj\\.vscode\\extensions\\ms-vscode.cpptools-1.22.9-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe",
   options = {
     detached = false,
   },
@@ -43,4 +43,38 @@ dap.configurations.cpp = {
 }
 
 -- TODO: Java DEBUG ADAPTER CONFIGURATION:
--- NOTE: WORK IN PROGRESS. COMING SOON.
+-- WARN: HANDLED BY THE NVIM-JDTLS PLUGINHANDLED BY THE NVIM-JDTLS PLUGIN.
+
+-- TODO: Python DEBUG ADAPTER CONFIGURATION:
+-- Helper function to get the Mason package path
+local function get_mason_package_path(package_name)
+  local mason_registry = require("mason-registry")
+  local package = mason_registry.get_package(package_name)
+  return package:get_install_path() -- We need to retrieve the path of where the package is installed, because it will contain everything we need.
+end
+
+-- Get debugpy path
+local debugpy_path = get_mason_package_path("debugpy")
+
+-- Python adapter setup
+dap.adapters.python = {
+  type = "executable",
+  command = debugpy_path .. "/venv/Scripts/python", -- NOTE: Using the packages version of python installed via it's virtual environment.
+  args = { "-m", "debugpy.adapter" }, -- NOTE: "debugpy" is actually a plugin for python, which is preferrably installed within a virtual environment.
+  options = {
+    detached = false, -- IMPORTANT: Specific to windows, we don't want other terminals opening up outside of the neovim session / powershell.
+  },
+}
+
+-- Python configuration
+dap.configurations.python = {
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    pythonPath = function() -- NOTE: Here we are actually using the version of python installed by mason for debugpy.
+      return debugpy_path .. "/venv/Scripts/python"
+    end,
+  },
+}
