fa4b6f25c340a9fc313b5ee69c5a1ecdd83b7c2b
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2024-10-21 00:08:27 +0100

    feat(plugin) Add "rainbow-csv" plugin.
    Added plugin for viewing csv data in colour coded format.

---
+++ lua/plugins/new/rainbow-csv.lua
@@ -0,0 +1,20 @@
+return {
+  "cameron-wags/rainbow_csv.nvim",
+  enabled = true, -- TESTING
+  config = true,
+  ft = {
+    "csv",
+    "tsv",
+    "csv_semicolon",
+    "csv_whitespace",
+    "csv_pipe",
+    "rfc_csv",
+    "rfc_semicolon",
+  },
+  cmd = {
+    "RainbowDelim",
+    "RainbowDelimSimple",
+    "RainbowDelimQuoted",
+    "RainbowMultiDelim",
+  },
+}
