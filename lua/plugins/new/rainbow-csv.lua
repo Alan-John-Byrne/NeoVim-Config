ecf7687f0857f3905f90cdee27aa5b4afd3c1500
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2025-01-13 01:50:34 +0000

    refactor(rainbow-csv) Add new plugin note.
    I've added a plugin note for the rainbow-csv viewer plugin.

--- lua/plugins/new/rainbow-csv.lua
+++ lua/plugins/new/rainbow-csv.lua
@@ -1,3 +1,4 @@
+-- PLUGIN: Installing the 'rainbow_csv' plugin. It provides better highlighting within neovim for csv files.
 return {
   "cameron-wags/rainbow_csv.nvim",
   enabled = true, -- TESTING
