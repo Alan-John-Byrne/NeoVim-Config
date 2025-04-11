50e920b6f836349667e0c080c2b0c82bfd8ec4cd
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2025-04-11 15:16:31 +0100

    feat(mini.surround): Remove extra 'replace' keybindings.
    I'm just removing these mappings because I don't find them useful.

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -1,14 +1,28 @@
 return {
   "echasnovski/mini.surround",
   opts = {
-    mappings = {
-      add = "gsa", -- Add surrounding
-      delete = "gsd", -- Delete surrounding
-      find = "gsf", -- Find surrounding (to the right)
-      find_left = "gsF", -- Find surrounding (to the left)
-      highlight = "gsh", -- Highlight surrounding
-      replace = "gsr", -- Replace surrounding
+    -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`:
+    custom_surroundings = nil,
+    -- Keymaps used:
+    mappings = {              -- DEF::
+      add = "gsa",            -- Add surrounding
+      delete = "gsd",         -- Delete surrounding
+      find = "gsf",           -- Find surrounding (to the right)
+      find_left = "gsF",      -- Find surrounding (to the left)
+      highlight = "gsh",      -- Highlight surrounding
       update_n_lines = "gsn", -- Update `n_lines`
+
+      -- IMPORTANT: replace = "gsr" Overwriting 'gsr' keymap in custom configuration. (Removing unecessary extra 'replace' keybindings)
     },
+    -- Number of lines within which surrounding is searched:
+    n_lines = 999999999
   },
+  config = function(_, opts)
+    local miniSurround = require("mini.surround")
+    -- Setting up using current options:
+    miniSurround.setup(opts)
+
+    -- NOTE: Custom keybind replace original 'replace' keymap.
+    vim.keymap.set("n", "gsr", function() miniSurround.replace() end);
+  end
 }
