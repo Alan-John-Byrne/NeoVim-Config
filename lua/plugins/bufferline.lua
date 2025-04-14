b3b394e1ac380d714a513efcf7fc1514f0bbe7bc
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2025-04-14 03:01:52 +0100

    fix(bufferline): Fix hard to see bufferline buffers.
    I've altered the appearance of the bufferline buffers and tabs to make them look fancy.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,6 +2,13 @@
 return {
   "akinsho/bufferline.nvim",
   event = "VeryLazy",
+  init = function()
+    -- WARN: For 'true color support' (when the plugin [the bufferline]
+    -- infers it's highlight groups from your chosen theme), the 'termguicolors'
+    -- option must be set prior to the 'bufferline' plugin being setup.
+    -- INFO: Need this for configuring highlight groups.
+    vim.opt.termguicolors = true
+  end,
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   keys = {
@@ -16,5 +23,290 @@ return {
     { "[B",         "<cmd>BufferLineMovePrev<cr>",             desc = "Move buffer prev" },
     { "]B",         "<cmd>BufferLineMoveNext<cr>",             desc = "Move buffer next" },
   },
-  opts = {}, -- Calling bufferline.setup({}) for initialization.
+  opts = {
+    options = { -- TODO: See ':h bufferline-configurations' for more options.
+      indicator = {
+        style = 'underline'
+      },
+      numbers = 'ordinal',
+      diagnostics = 'nvim_lsp',
+      -- IMPORTANT: Changing the below style could mean having
+      -- to alter seperator colours.
+      separator_style = 'slant' -- TODO: See ':h bufferline-styling' for more info on styles.
+    },
+    highlights = {              -- TODO: See ':h bufferline-highlight' for more info on highlight groups.
+      --WARN:
+      -- For when deciding on which highlight groups:
+      -- fg means 'Text',
+      -- bg means 'Background behind the Text'
+      -- IMPORTANT: Incorrect Setting:
+      -- fill = {
+      --   fg = 'CursorLine',
+      --   bg = 'CursonColumn'
+      -- },
+      -- INFO:
+      -- Unpopulated space on the bufferline.
+      fill = {
+        bg = { -- Only 'bg' applies here.
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- Currently opened buffer seperator
+      -- colours.
+      separator_selected = {
+        fg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- Seperator colours for buffers you
+      -- are NOT currently viewing.
+      separator = {
+        fg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- Seperator colours for the previously
+      -- opened buffer.
+      separator_visible = {
+        fg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the buffer you're
+      -- currently viewing, is.
+      buffer_selected = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+        bold = true,
+        italic = true,
+      },
+      -- INFO:
+      -- The colour of the buffers you are NOT
+      -- currently viewing.
+      background = {
+        fg = { -- The text of those buffers.
+          attribute = "fg",
+          highlight = "WinBar"
+        },
+        bg = { -- The background of those buffers.
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- The previously entered buffer.
+      buffer_visible = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the 'exit' button will be
+      -- on the buffer you're currently viewing.
+      close_button_selected = {
+        fg = {
+          attribute = "fg",
+          highlight = "WarningMsg"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the 'exit' button will be
+      -- when you are NOT looking at it's buffer.
+      close_button = {
+        fg = {
+          attribute = "fg",
+          highlight = "WarningMsg"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- The colour the 'exit' button of
+      -- the previously opened buffer.
+      close_button_visible = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the number of the buffer will be
+      -- when you are currently viewing that buffer.
+      numbers_selected = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the number of the buffer will be
+      -- when you are NOT viewing that buffer.
+      numbers = {
+        fg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- The colour the number of the buffer
+      -- opened previously.
+      numbers_visible = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the modified icon will be
+      -- when you are currently viewing that changed
+      -- / altered buffer.
+      modified_selected = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour the modified icon will be
+      -- when you are NOT viewing that changed
+      -- / altered buffer.
+      modified = {
+        fg = {
+          attribute = "fg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- The colour of the tab you are
+      -- currently viewing.
+      tab_selected = {
+        fg = {
+          attribute = "fg",
+          highlight = "PMenu"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+      },
+      -- INFO:
+      -- The colour of the tabs you are
+      -- NOT currently viewing.
+      tab = {
+        fg = {
+          attribute = "fg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- The colour of the tab seperator of the
+      -- tab you are currently viewing.
+      tab_separator_selected = {
+        fg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "PMenu"
+        },
+        sp = {
+          attribute = "fg",
+          highlight = "WinBar"
+        },
+        underline = false -- Include or don't include an underline.
+      },
+      -- INFO:
+      -- The colour of the tab seperators of the
+      -- tabs you are NOT currently viewing.
+      tab_separator = {
+        fg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+      -- INFO:
+      -- The close button of the currently
+      -- opened tab.
+      tab_close = {
+        fg = {
+          attribute = "fg",
+          highlight = "WarningMsg"
+        },
+        bg = {
+          attribute = "bg",
+          highlight = "WinBar"
+        },
+      },
+    }
+  }, -- Calling bufferline.setup({}) for initialization.
 }
--- lua/plugins/gruvbox-colourscheme.lua
+++ lua/plugins/gruvbox-colourscheme.lua
@@ -1,16 +1,22 @@
 -- PLUGIN: The 'gruvbox.nvim' plugin provides a much loved colorscheme.
 return {
   "ellisonleao/gruvbox.nvim",
-  --  lazy = false,
+  lazy = false,                    -- INFO: Illustrating that the colourscheme MUST NOT be lazy.
   config = function()
-    vim.cmd.colorscheme("gruvbox")
-
-    -- NOTE: Overriding global highlight-groups to remove solid background. (Opacity taken care of by WezTerm)
-    vim.api.nvim_set_hl(0, 'Normal', { bg = nil })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = nil })
-    vim.api.nvim_set_hl(0, 'NonText', { bg = nil })
-    vim.api.nvim_set_hl(0, 'CursorLine', { bg = nil })
-    vim.api.nvim_set_hl(0, 'SignColumn', { bg = nil })
-    vim.api.nvim_set_hl(0, 'WinBar', { bg = nil })
+    vim.cmd.colorscheme("gruvbox") -- Loading the colourscheme.
+    -- IMPORTANT: The following must be done AFTER loading the colourscheme.
+    if (true) then
+      -- WARN: Controlling nvim transparency, and managing suitable colour combinations for
+      -- plugins such as 'bufferline.nvim'.
+      -- NOTE: Overriding global highlight-groups to remove
+      -- solid background. (Background Opacity taken care of
+      -- by *WezTerm*).
+      vim.api.nvim_set_hl(0, 'Normal', { bg = nil })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = nil })
+      vim.api.nvim_set_hl(0, 'SignColumn', { bg = nil })
+      vim.api.nvim_set_hl(0, 'CursorLine', { bg = nil })
+      -- REMEMBER: 'vim.api' is Neovim's API. It's used by plugins,
+      -- Lua, and vimscript.
+    end
   end,
 }
