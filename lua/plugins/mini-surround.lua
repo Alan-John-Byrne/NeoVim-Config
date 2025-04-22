7122e373c7587b5520c9d301a6c1125f72646540
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2025-04-22 16:50:02 +0100

    fix(mini-surround): Mitigate user input issue.
    Originally I tried to modify the plugin spec so the user would only have to worry about changing the surrounding under the cursor. This is already achieved in the original
    spec, however is a little unclear. I've reverted back to this spec with a few added comments for clarity, as these 'extra' options do prove useful from time to time. Plus
    there was an issue with my change where the user wouldn't be prompted again for another surrounding to replace or delete, after first using the keymap. This is because under
    the hood the plugin uses neovim operators and caching to remember what the user have previously selected. This isn't good so I'm reverting with a better understanding of the plugin.

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -5,26 +5,15 @@ return {
     custom_surroundings = nil,
     -- Keymaps used:
     mappings = {              -- DEF::
-      add = "gsa",            -- Add surrounding
-      find = "gsf",           -- Find surrounding (to the right)
-      find_left = "gsF",      -- Find surrounding (to the left)
-      highlight = "gsh",      -- Highlight surrounding
-      update_n_lines = "gsn", -- Update `n_lines`
-
-      -- IMPORTANT: - Overwriting keymaps. (Removing unecessary / 'extra' keybindings)
-      -- replace = "gsr" - Replace surrounding
-      -- delete = "gsd" - Delete surrounding
+      add = 'gsa',            -- Add surrounding
+      find = 'gsf',           -- Find surrounding (to the right)
+      find_left = 'gsF',      -- Find surrounding (to the left)
+      highlight = 'gsh',      -- Highlight surrounding
+      update_n_lines = 'gsn', -- Update `n_lines`
+      replace = 'gsr',        -- Replace surrounding (Use 'gsr' and ignore "left or right of the cursor" options for 'under cursor' selection.)
+      delete = 'gsd'          -- Delete surrounding (Use 'gsd'... *same as replace*)
     },
     -- Number of lines within which surrounding is searched:
     n_lines = 999999999
   },
-  config = function(_, opts)
-    local miniSurround = require("mini.surround")
-    -- Setting up using current options:
-    miniSurround.setup(opts)
-
-    -- NOTE: Custom keybinds to replace original keymaps Overwritten above.
-    vim.keymap.set("n", "gsr", function() miniSurround.replace() end);
-    vim.keymap.set("n", "gsd", function() miniSurround.delete() end);
-  end
 }
